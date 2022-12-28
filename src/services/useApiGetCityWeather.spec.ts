7614dedf2c7868ee3565dd279ac6e54aee295308
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2022-12-28 08:57:05 +0100

    chore: change test description

--- src/services/useApiGetCityWeather.spec.ts
+++ src/services/useApiGetCityWeather.spec.ts
@@ -1,4 +1,7 @@
 import { useApiGetClimate } from "services/useApiGetCityWeather";
+import { MOCK } from "constants/MOCK_DATA_SERVICE-London";
+const axios = require("axios");
+jest.mock("axios");
 
 const makeSut = () => ({
   sut: useApiGetClimate,
@@ -8,8 +11,9 @@ const makeSut = () => ({
 
 describe(useApiGetClimate.name, () => {
   test("shloud be return data when value is London", () => {
-    // const { sut } = makeSut();
-    // const result = sut("London");
-    // expect(result).toBe(MOCK);
+    const { sut } = makeSut();
+    axios.get.mockResolvedValue(MOCK);
+    const result = sut("London");
+    expect(result).toBe(MOCK);
   });
 });
