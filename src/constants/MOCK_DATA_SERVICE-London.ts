f888b05428e80a7b79e41ad96b2e351ea92c123f
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2022-12-28 08:44:28 +0100

    chore: change test description

---
+++ src/constants/MOCK_DATA_SERVICE-London.ts
@@ -0,0 +1,107 @@
+export const MOCK = {
+    "status": "success",
+    "fetchStatus": "idle",
+    "isLoading": false,
+    "isSuccess": true,
+    "isError": false,
+    "isInitialLoading": false,
+    "data": {
+        "data": {
+            "coord": {
+                "lon": -0.1257,
+                "lat": 51.5085
+            },
+            "weather": [
+                {
+                    "id": 500,
+                    "main": "Rain",
+                    "description": "light rain",
+                    "icon": "10n"
+                }
+            ],
+            "base": "stations",
+            "main": {
+                "temp": 8.36,
+                "feels_like": 5.1,
+                "temp_min": 7.64,
+                "temp_max": 9.33,
+                "pressure": 1013,
+                "humidity": 90
+            },
+            "visibility": 10000,
+            "wind": {
+                "speed": 6.17,
+                "deg": 210
+            },
+            "rain": {
+                "1h": 0.17
+            },
+            "clouds": {
+                "all": 75
+            },
+            "dt": 1672171318,
+            "sys": {
+                "type": 2,
+                "id": 2075535,
+                "country": "GB",
+                "sunrise": 1672128354,
+                "sunset": 1672156610
+            },
+            "timezone": 0,
+            "id": 2643743,
+            "name": "London",
+            "cod": 200
+        },
+        "status": 200,
+        "statusText": "OK",
+        "headers": {
+            "content-length": "478",
+            "content-type": "application/json; charset=utf-8"
+        },
+        "config": {
+            "transitional": {
+                "silentJSONParsing": true,
+                "forcedJSONParsing": true,
+                "clarifyTimeoutError": false
+            },
+            "adapter": [
+                "xhr",
+                "http"
+            ],
+            "transformRequest": [
+                null
+            ],
+            "transformResponse": [
+                null
+            ],
+            "timeout": 0,
+            "xsrfCookieName": "XSRF-TOKEN",
+            "xsrfHeaderName": "X-XSRF-TOKEN",
+            "maxContentLength": -1,
+            "maxBodyLength": -1,
+            "env": {},
+            "headers": {
+                "Accept": "application/json, text/plain, */*"
+            },
+            "method": "get",
+            "url": "https://api.openweathermap.org/data/2.5/weather?q=Londres&appid=2a70c4d623654daecbe0ab9704c390ad&units=metric"
+        },
+        "request": {}
+    },
+    "dataUpdatedAt": 1672171540735,
+    "error": null,
+    "errorUpdatedAt": 0,
+    "failureCount": 0,
+    "failureReason": null,
+    "errorUpdateCount": 0,
+    "isFetched": true,
+    "isFetchedAfterMount": true,
+    "isFetching": false,
+    "isRefetching": false,
+    "isLoadingError": false,
+    "isPaused": false,
+    "isPlaceholderData": false,
+    "isPreviousData": false,
+    "isRefetchError": false,
+    "isStale": false
+}
